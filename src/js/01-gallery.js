7cf07eb9c1369d9bcbd01ff9197e733c01671d57
liudmila05 <ol.liuda.94@gmail.com>
2022-10-22 10:31:35 +0300

    Task 1 && 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const lightbox = document.querySelector(".gallery");
+const images = galleryItems
+  .map(
+    (image) =>
+      ` <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                    class="gallery__image"
+                    src="${image.preview}"
+                    data-source="${image.original}"
+                    alt="${image.description}"
+                />
+            </a>
+        </div>`
+  )
+  .join("");
+
+lightbox.innerHTML = images;
+
+lightbox.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return;
+  const source = e.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src="${source}" width="800" height="600">`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", (event) => {
+          if (event.key === "Escape") instance.close();
+        });
+      },
+    }
+  );
+
+  instance.show();
+});
+
 console.log(galleryItems);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const newLightBox = document.querySelector(".gallery");
+const images = galleryItems
+    .map(
+        (image) =>
+            `<a class="gallery__item" href="${image.original}">
+                <img class="gallery__image"    
+                src="${image.preview}"
+                alt="${image.description}"
+                title="${image.description}"
+                />
+            </a>`
+    )
+    .join("");
+
+newLightBox.innerHTML = images;
+
+new SimpleLightbox(".gallery a", { captionDelay: 250 });
 
 console.log(galleryItems);
